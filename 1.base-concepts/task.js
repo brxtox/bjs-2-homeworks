cd18cc59a246e7be03cb024dbb81df860aef5564
Abdellah Shenni <brxtox@gmail.com>
2024-10-26 12:23:17 +0300

    Основные понятия Задача 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  // let d = b ** 2 - 4 * a * c;
+  let d = Math.pow(b, 2) - 4 * a * c;
+  let r;
+  let r1;
+  let r2;
+  if (d < 0) {
+    return arr;
+  } else if (d === 0) {
+    r = -b / (2 * a);
+    arr.push(r);
+  } else {
+    r1 = (-b + Math.sqrt(d)) / (2 * a);
+    r2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(r1, r2)
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
 }
