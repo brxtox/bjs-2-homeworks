4e42b0ff41c70c1cf768db721cf5b29dc9f323ec
Abdellah Shenni <brxtox@gmail.com>
2024-11-08 00:32:29 +0300

    (поправил 1 задачу и 2)

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,24 +2,23 @@
 
 function solveEquation(a, b, c) {
   let arr = [];
-  let d = Math.pow(b, 2) - 4 * a * c;
-  let r, r1, r2;
-  if (d === 0) {
-    r = -b / (2 * a);
-    arr.push(r);
-  } else if (d > 0) {
-    r1 = (-b + Math.sqrt(d)) / (2 * a);
-    r2 = (-b - Math.sqrt(d)) / (2 * a);
-    arr.push(r1, r2)
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
+  if (discriminant === 0) {
+    let root = -b / (2 * a);
+    arr.push(root);
+  } else if (discriminant > 0) {
+    let rootNumberOne = (-b + Math.sqrt(discriminant)) / (2 * a);
+    let rootNumberTwo = (-b - Math.sqrt(discriminant)) / (2 * a);
+    arr.push(rootNumberOne, rootNumberTwo);
   }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let P = (percent / 100) / 12;
-  let S = amount - contribution;
-  let monthlyAmount = S * (P + (P / ((Math.pow(1 + P, countMonths)) - 1)));
+  let monthlyPercent = (percent / 100) / 12;
+  let loanPrincipal = amount - contribution;
+  let monthlyAmount = loanPrincipal * (monthlyPercent + (monthlyPercent / ((Math.pow(1 + monthlyPercent, countMonths)) - 1)));
   let totalAmount = monthlyAmount * countMonths;
-  totalAmount = totalAmount.toFixed(2);
-  return +totalAmount;
+
+  return +totalAmount.toFixed(2);
 }
