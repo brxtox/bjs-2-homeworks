f09e075a4399587cef96161c85554550f3104a93
Abdellah Shenni <brxtox@gmail.com>
2024-11-10 03:59:47 +0300

    Initial commit «Функции» все задачи

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,78 @@
-function getArrayParams(...arr) {
 
+function getArrayParams(...arr) {
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let avg = +((arr.reduce(function (previousValue, currentValue) {
+    return previousValue + currentValue;
+  })) / arr.length).toFixed(2);
   return { min: min, max: max, avg: avg };
 }
 
-function summElementsWorker(...arr) {
+/* function isEmpty(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+} */
 
+function summElementsWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  return arr.reduce(function (previousValue, currentValue) {
+    return previousValue + currentValue;
+  })
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  if (arr.length === 0) {
+    return 0;
+  }
+  return Math.max(...arr) - Math.min(...arr);
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  if (arr.length === 0) {
+    return 0;
+  }
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+  arr.map((num) => {
+    if (num % 2 === 0) {
+      sumEvenElement += num;
+    } else if (num % 2 !== 0) {
+      sumOddElement += num;
+    }
+  })
+  return sumEvenElement - sumOddElement;
 }
+
 
 function averageEvenElementsWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+      countEvenElement++;
+    } else {
+      continue;
+    }
+  }
+  return sumEvenElement / countEvenElement;
 }
 
-function makeWork (arrOfArr, func) {
+function makeWork(arrOfArr, func) {
+  let maxWorkerResult = -Infinity;
 
+  for (let array of arrOfArr) {
+    const funcResult = func(...array);
+    if (!!(funcResult > maxWorkerResult)) {
+      maxWorkerResult = funcResult;
+    }
+  }
+  return maxWorkerResult;
 }
